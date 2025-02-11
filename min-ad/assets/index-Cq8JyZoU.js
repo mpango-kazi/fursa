ca58df75f7b545613432ab699b300195fb797217
Bkmillanzi <bkmillanzi@hotmail.com>
2025-02-11 12:59:05 +0300

    Delete min-ad directory

--- min-ad/assets/index-Cq8JyZoU.js
+++
@@ -1,1 +0,0 @@
-import{d as a}from"./features-animation-BOIsEcAV.js";import"./index-Drimx7Tn.js";var t=a;export{t as default};
--- min-ad/assets/index.ClqzifZ5.js
+++
@@ -1,1 +0,0 @@
-import{d as a}from"./features-animation.7EMagNIQ.js";import"./index.jSojJ5cM.js";var t=a;export{t as default};
